cdf272275b6b318ea668ba1625945c5d2b394b12
Dennis Lee <dennisdjlee@yahoo.com>
2020-04-28 19:37:56 -0300

    add .well-known/pki-validation/starfield.html

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,6 +15,7 @@ import ContractDetails from '~/pages/Contract/Details';
 import Profile from '~/pages/Profile';
 import Calendar from '~/pages/Calendar/List';
 import CalendarEdit from '~/pages/Calendar/Edit'
+import pki from '~/pages/pki/pki'
 
 export default function Routes() {
 
@@ -34,6 +35,7 @@ export default function Routes() {
       <Route path="/calendar" exact component={Calendar} isPrivate/>
       <Route path="/calendar/edit" exact component={CalendarEdit} isPrivate/>
       <Route path="/scan" exact component={Scan} />
+      <Route path="/.well-known/pki-validation/starfield.html" exact component={pki} />
 
       <Route path="/" component={() => <h3>404 Not Found</h3>} />
     </Switch>
